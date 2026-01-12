0523fb26bd771ddce4066d50c75ac79fdf7b1f07
Ebikakpou <Ebikakpou@gmail.com>
2026-01-13 00:42:36 +0100

    Phase 4: Provision GCP Compute Engine with Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "app_vm" {
+  name         = "devops-app-vm"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["http-server"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
